77bc1f330b890b4c938241347f4d326312ab0dc3
herraphfx <herraph@gmail.com>
2022-04-01 14:50:20 +0400

    update restart

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// START PAGE
-
-// Audio Button - music
-// window.onload = function() {
-//   document.getElementById("audio_tag").play();
-// }​
-function audio_button() {
-    let audio_button_change = document.getElementById('audio');
-    let audio_control = document.getElementById('audio_tag')
-
-    if (audio_button_change.className === 'bi bi-volume-mute-fill fa-2x') {
-        audio_button_change.setAttribute('class', 'bi bi-volume-up-fill fa-2x');
-        audio_control.play();
-    }
-    else {
-        audio_button_change.setAttribute('class', 'bi bi-volume-mute-fill fa-2x');
-        audio_control.pause();
-    }
-}
 
 
 

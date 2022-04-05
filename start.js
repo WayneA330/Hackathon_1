9ee459413c44c2bfd89a8d8825816336dfa3a8c6
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-04-05 08:28:36 +0400

    sound fixed and chasing implimented

--- start.js
+++ start.js
@@ -3,13 +3,13 @@ function audio_button() {
     let audio_button_change = document.getElementById('audio');
     let audio_control = document.getElementById('audio_tag')
 
-    if (audio_button_change.className === 'bi bi-volume-mute-fill fa-2x') {
-        audio_button_change.setAttribute('class', 'bi bi-volume-up-fill fa-2x');
-        audio_control.play();
-    }
-    else {
+    if (audio_button_change.className === 'bi bi-volume-up-fill fa-2x') {
         audio_button_change.setAttribute('class', 'bi bi-volume-mute-fill fa-2x');
         audio_control.pause();
+    }
+    else {
+        audio_button_change.setAttribute('class', 'bi bi-volume-up-fill fa-2x');
+        audio_control.play();
     }
 }
 
